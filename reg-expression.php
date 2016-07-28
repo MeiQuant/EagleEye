a916e6fd93d5f88d5b738dfc7af532a4a254b873
wanghongjun <1583291613@qq.com>
2016-07-28 18:02:33 +0800

    add

--- reg-expression.php
+++ reg-expression.php
@@ -3,10 +3,27 @@
 /*
  *  ==============type===1001========**真融宝总交易额
  */
+
 $content = file_get_contents('https://www.zhenrongbao.com');
-$pattern = '/<p><span class="icon"><\/span>累计投资总额：<span>￥(.*?)<\/span><\/p>/';
+$pattern = '/<p><span class="icon"><\/span>累计投资总额：<span>￥(.*?)<\/span><\/p>/i';
 preg_match_all($pattern, $content, $matches);
 if (!empty($matches) && is_array($matches)) {
     return str_replace(',', '', $matches[1][0]);
 }
+
+
+/*
+ *  ==============type===1002========**真融宝投资人数
+ */
+
+$content = file_get_contents('https://www.zhenrongbao.com');
+$pattern = '/投资人数：<span>(.*?)人<\/span>/i';
+preg_match_all($pattern, $content, $matches);
+if (!empty($matches) && is_array($matches)) {
+    return str_replace(',', '', $matches[1][0]);
+}
+
+
+
+
 
