f97ac157ff33c5d0f45132e49a15297a74da8819
wanghongjun <1583291613@qq.com>
2016-07-28 09:38:21 +0800

    fix

--- app/Console/Commands/GetCronTasks.php
+++ app/Console/Commands/GetCronTasks.php
@@ -38,8 +38,11 @@ class GetCronTasks extends Command
     public function handle()
     {
         /**
-         *  将服务器上cron的脚本读出来,存取到数据库中,使之在后台可以看到
+         *  1, 将服务器上cron的脚本读出来,存取到数据库中,使之在后台可以看到
+         *
+         *  2, 初始化之后,每当cron有变动时才会触发该脚本
          */
+
         
     }
 }
