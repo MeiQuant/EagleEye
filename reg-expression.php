8df2aa2808bf608337b83f287fd4cfdcd21c10b3
wanghongjun <1583291613@qq.com>
2016-07-31 17:20:04 +0800

    添加规则

--- reg-expression.php
+++ reg-expression.php
@@ -24,6 +24,45 @@ if (!empty($matches) && is_array($matches)) {
 }
 
 
+/*
+ * ==============type===1003========**真融宝为用户赚取的收益
+ */
+
+return 0;
+
+
+/**
+ *  ==============type===1004========**真融宝定期产品总投资额
+ */
+
+$contents = file_get_contents('https://www.zhenrongbao.com/dingqi');
+$pattern = '/<p class="line-1">(.*)<\/p>/i';
+preg_match_all($pattern, $contents, $matches);
+if (!empty($matches) && is_array($matches)) {
+    return  str_replace('￥', '', str_replace(',', '', $matches[1][1]));
+}
+
+/**
+ *  ==============type===1005========**真融宝定期产品投资总人数
+ */
+$contents = file_get_contents('https://www.zhenrongbao.com/dingqi');
+$pattern = '/<p class="line-1">(.*)<\/p>/i';
+preg_match_all($pattern, $contents, $matches);
+if (!empty($matches) && is_array($matches)) {
+    return  $matches[1][0];
+}
+
+
+/**
+ *  ==============type===1006========**真融宝定期产品帮用户赚取的金额
+ */
+
+$contents = file_get_contents('https://www.zhenrongbao.com/dingqi');
+$pattern = '/<p class="line-1">(.*)<\/p>/i';
+preg_match_all($pattern, $contents, $matches);
+if (!empty($matches) && is_array($matches)) {
+    return  str_replace('￥', '', str_replace(',', '', $matches[1][2]));
+}
 
 
 
