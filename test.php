bd0d112b2dcd0c04eb06943b45561dc40db65360
hongjunw <1583291613@qq.com>
2016-07-31 23:21:04 +0800

    fix asset shell

--- test.php
+++ test.php
@@ -1,12 +1,33 @@
 <?php
 
-
-
 $content = file_get_contents('https://www.zhenrongbao.com/plat/plat_assemble?pid=2&current_page=1&_access_token=&platform=pc');
 $content = json_decode($content, true);
-$pageCount = 0;
+$pagePlatCount = 0;
 if (!empty($content) && is_array($content)) {
-    $pageCount = $content['data']['page_count'];
+    $pagePlatCount = $content['data']['page_count'];
 }
 
+$i = 0;
+$pagePlatData = ''; //每一页的数据
+$results = [];
+
+for($i=0; $i<=$pagePlatCount; $i++) {
+    $pagePlatContent = file_get_contents('https://www.zhenrongbao
+    .com/plat/plat_assemble?pid=2&current_page='. $i .'&_access_token=&platform=pc');
+    $pagePlatContent  = $pagePlatContent['data']['credit_plat'];
+    $platformName = $pagePlatContent['plat_name'];
+    $platPercent = $pagePlatContent['plat_amount_percent'];
+    $platNum     = $pagePlatContent['plat_credit_amount']; //资产数
+    //查询相应平台下面的所有资产,首先也是获取资产的总数
+    $pageAssetData = file_get_contents('https://www.zhenrongbao.com/plat/plat_credit_assemble?pid=2&current_page=1&
+    credit_plat_id='. $pagePlatContent['credit_plat_id'].'&_access_token=&platform=pc');
+    $pageAssetCount = $pageAssetData['data']['page_count'];
+    for ($j=0; $j<=$pageAssetCount; $j++) {
+        $pageAssetContent = file_get_contents('https://www.zhenrongbao
+        .com/plat/plat_credit_assemble?pid=2&current_page=1&
+    credit_plat_id='. $pagePlatContent['credit_plat_id'].'&_access_token=&platform=pc');  //某个平台下某个资产的相关信息
+        
+    }
+
+}
 
