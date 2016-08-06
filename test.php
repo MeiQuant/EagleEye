58514057c9102d772c407438ed7dabc5602cfba6
wanghongjun <1583291613@qq.com>
2016-08-06 23:38:10 +0800

    他妈的,米袋的数据怎么抓不到

--- test.php
+++ test.php
@@ -1,11 +1,9 @@
 <?php
 
-$contents = file_get_contents('https://www.zhenrongbao.com/index/demand');
-$huoqiContents = file_get_contents('https://www.zhenrongbao.com/plat/assemble?pid=1');
-$pattern = '/<p class="line-1">(.*)<\/p>/i';
-$huoqiPattern = '/<p class="number"><span>(.*)<\/span>/i';
+$contents = file_get_contents('https://www.midaijihua.com');
+$pattern = '/<span class="pull-right">(.*?)<\/span>/i';
 preg_match_all($pattern, $contents, $matches);
-preg_match_all($huoqiPattern, $huoqiContents, $huoqiMatches);
+print_r($matches);die;
 $result =  [
     'product_id' => 2,
     'total_invest_amounts' => str_replace('￥', '', str_replace(',', '', $matches[1][1])),
@@ -16,4 +14,22 @@ $result =  [
 ];
 
 print_r($result);die;
+
+$contents = file_get_contents('https://www.zhenrongbao.com/dingqi');
+$dingqiContents = file_get_contents('https://www.zhenrongbao.com/plat/assemble?pid=2');
+$pattern = '/<p class="line-1">(.*)<\/p>/i';
+$dingqiPattern = '/<p class="number"><span>(.*)<\/span>/i';
+preg_match_all($pattern, $contents, $matches);
+preg_match_all($dingqiPattern, $dingqiContents, $dingqiMatches);
+return [
+    'product_id' => 1,
+    'total_invest_amounts' => str_replace('￥', '', str_replace(',', '', $matches[1][1])),
+    'total_invest_persons' => $matches[1][0],
+    'total_profits' => str_replace('￥', '', str_replace(',', '', $matches[1][2])),
+    'asset_count' => $dingqiMatches[1][2],
+    'plat_count' => $dingqiMatches[1][1]
+];
+
+
+
 
